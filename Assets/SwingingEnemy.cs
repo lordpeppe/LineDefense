0ea23e5a685f702c5c3af05bf3fabe770fab5a91
Daniel Bross <daniel@Daniels-MacBook-Pro.local>
2015-12-08 00:12:57 +0100

    New scene with scrolling shooter + iTween path following package + shooting and flying spaceship

---
+++ Assets/SwingingEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingingEnemy : Enemy 
+{
+	[SerializeField]
+	private GameObject explosionPrefab;
+
+	void Start () 
+	{
+		currentHealth = initHealth;
+	}
+	
+	void Update () 
+	{
+		if (IsDestroyed ()) 
+		{
+			StartCoroutine(Explode());
+		}
+	}
+
+	IEnumerator Explode()	
+	{
+		gameObject.SetActive(false);
+//		GameObject text = Instantiate (xpText, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity) as GameObject;
+//		text.GetComponent<XPText> ().Show (xp);
+		GameObject explosion = Instantiate (explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+		yield return new WaitForSeconds (0.8f);
+		Destroy (explosion);
+	}
+}
