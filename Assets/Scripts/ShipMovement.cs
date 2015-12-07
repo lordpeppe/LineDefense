a97686b1c22f2baa630b00b56d8a7a0863b75aee
Daniel Bross <daniel@Daniels-MacBook-Pro.local>
2015-12-07 22:55:33 +0100

    New scene with scrolling shooter + iTween path following package + shooting and flying spaceship

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	[SerializeField]
+	private KeyCode up, down, left, right, shoot;
+	private SpriteRenderer spriteRenderer;
+
+	void Start () 
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKey (up)) 
+		{
+			transform.Translate(Vector2.up * 10 * Time.deltaTime);
+		} 
+		else if (Input.GetKey (down)) 
+		{
+			transform.Translate(Vector2.up * -10 * Time.deltaTime);
+		} 
+		else if (Input.GetKey (left)) 
+		{
+			if (transform.position.x > Camera.main.ScreenToWorldPoint (new Vector2(50, 9)).x)
+				transform.Translate (Vector2.right * -10 * Time.deltaTime);
+		} 
+		else if (Input.GetKey (right)) 
+		{
+			if (transform.position.x < Camera.main.ScreenToWorldPoint (new Vector2(Camera.main.pixelWidth, 9)).x - spriteRenderer.bounds.extents.x)
+				transform.Translate (Vector2.right * 10 * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/ShipShooting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipShooting : MonoBehaviour 
+{
+	[SerializeField]
+	private KeyCode shoot;
+
+	private Resource projectilePool;
+
+	[SerializeField]
+	private Rigidbody2D defaultProjectile;
+
+	[SerializeField]
+	private int projectileLimit = 20;
+
+	private float speed = 15;
+
+	void Start () 
+	{
+		projectilePool = new Resource(defaultProjectile, projectileLimit, transform.position);
+	}
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown (shoot)) 
+		{
+			Shoot ();
+		}
+	}
+
+	void Shoot()
+	{
+		Rigidbody2D projectile = projectilePool.getNext();
+		projectile.transform.position = transform.position - new Vector3(0, 0.7f, 0);
+		projectile.gameObject.SetActive(true);
+		projectile.velocity = new Vector2(speed, 0);
+
+		Rigidbody2D projectile2 = projectilePool.getNext();
+		projectile2.transform.position = transform.position + new Vector3(0, 0.7f, 0);
+		projectile2.gameObject.SetActive(true);
+		projectile2.velocity = new Vector2(speed, 0);
+	}
+
+}
