5880349355cbe4023f89beeb55150c58c5f5a165
NielsB <niels_bross@hotmail.com>
2015-12-10 18:01:28 +0100

    Revert "Revert "Revert "Revert "tits up""""
    This reverts commit 2f1f3a4677b99a15c3c927ebc109daa97ba765d0.

---
+++ Assets/Scripts/UI/Map.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Map  {
+
+    List<Objective> objectives;
+    public List<Objective> Objectives { get { return objectives; } }
+
+    Vector2 curPos;
+    public Vector2 CurPos { get { return curPos; } }
+
+
+    public Map(List<Objective> objectiveList, Vector2 initPos)
+    {
+        objectives = objectiveList;
+        curPos = initPos;
+    }
+
+    public void ActivateObjective(Objective obj)
+    {   
+        foreach (Objective o in objectives)
+        {
+            if (o.Equals(obj))
+            {
+                o.Active = true;
+                break;
+            }
+        }
+    }
+    
+
+
+}
---
+++ Assets/Scripts/Weapons/shooting/AbstractShooter.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractShooter : MonoBehaviour {
+
+    protected Resource projectilePool;
+
+    [SerializeField]
+    protected Rigidbody2D defaultProjectile;
+
+    [SerializeField]
+    protected int projectileLimit = 20;
+
+    [SerializeField]
+    protected float speed = 15;
+
+    protected bool CanShoot { get; set; }
+
+    [SerializeField]
+    protected float cooldownTime = 0.2f;
+
+
+    public void Init()
+    {
+        CanShoot = true;
+        transform.position = transform.parent.position;
+        projectilePool = new Resource(defaultProjectile, projectileLimit, transform.position);
+    }
+
+    void Update()
+    {
+
+
+    }
+
+    public void Shoot()
+    {
+        if (CanShoot)
+        {
+            AuxShoot(projectilePool, speed);
+            StartCoroutine(BulletCooldown());
+            CanShoot = false;
+        }
+    }
+
+    public abstract void AuxShoot(Resource projectilePool, float speed);
+
+    IEnumerator BulletCooldown()
+    {
+        yield return new WaitForSeconds(cooldownTime);
+        CanShoot = true;
+    }
+}
