361f2859a009d83f7120b19873e01c5756ddd0b1
NielsB <niels_bross@hotmail.com>
2015-12-10 17:58:34 +0100

    Revert "Revert "tits up""
    This reverts commit 496cfb98b56ac7a1cd122d95baeb4efcdebfa9d2.

---
+++ Assets/Scripts/Enemies/KamikazeEnemy.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KamikazeEnemy : Enemy 
+{
+	[SerializeField]
+	private Transform hq;
+	[SerializeField]
+	private GameObject explosionPrefab;
+
+	void Start () 
+	{
+		xp = 5;
+		initHealth = 5;
+		speed = 3;
+		if (hq == null)
+			hq = GameObject.FindGameObjectWithTag ("HQ").transform;
+	}
+
+	void Awake()
+	{
+		if (hq == null)
+			hq = GameObject.FindGameObjectWithTag ("HQ").transform;
+	}
+
+	void Update()
+	{
+		if (!IsDestroyed())
+		{
+			Move(hq.transform.position.x , hq.transform.position.y);
+		}
+		else
+		{
+			GameObject.FindGameObjectWithTag ("level").GetComponent<LevelStatus>().DecrementHealth();
+			StartCoroutine(Explode());
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.tag == "HQ")
+		{
+			other.gameObject.GetComponent<HQ>().TakeDamage(damage);
+			StartCoroutine(Explode());
+		}
+	}
+
+	IEnumerator Explode()	
+	{
+		gameObject.SetActive(false);
+		GameObject text = Instantiate (xpText, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity) as GameObject;
+		text.GetComponent<XPText> ().Show (xp);
+		GameObject explosion = Instantiate (explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+		yield return new WaitForSeconds (0.8f);
+		Destroy (explosion);
+	}
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour 
+{
+	public static LevelManager levelManager;
+
+	public GameObject canvas;
+    public Objective CurObjective { get; set; }
+	/* Playe data */
+	public int GoldCoins { get; set; }
+    public Map ActiveMap { get; set; }
+
+	void Awake ()
+	{
+		if (levelManager == null) 
+		{
+			GoldCoins = 100;
+			DontDestroyOnLoad (gameObject);
+			levelManager = this;
+		} 
+		else if(levelManager != this)
+		{
+			Destroy (gameObject);
+		}
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+    public void ReloadMap(string map)
+    {
+        Application.LoadLevel(map);
+        foreach(Objective o in levelManager.ActiveMap.Objectives)
+        {
+            o.gameObject.SetActive(true);
+        }
+    }
+
+    public void SetMap(Map map)
+    {
+        levelManager.ActiveMap = map;
+    }
+
+    public void SetLevelAndLoad(Objective obj)
+    {
+        Application.LoadLevel(obj.Destination);
+        CurObjective = obj;
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("objective"))
+        {
+            go.SetActive(false);
+        }
+    }
+}
