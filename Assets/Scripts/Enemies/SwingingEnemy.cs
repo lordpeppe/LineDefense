99e9ab4eea8992d46c7af0d46193684af8e8c697
NielsB <niels_bross@hotmail.com>
2015-12-10 18:01:35 +0100

    Revert "Revert "Revert "Revert "tits up""""
    This reverts commit 6a783211028b571539d385e261d6e27c0ab3256c.

---
+++ Assets/Scripts/Enemies/SwingingEnemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwingingEnemy : Enemy 
+{
+	[SerializeField]
+	private GameObject explosionPrefab;
+
+	void Start () 
+	{
+		currentHealth = initHealth;
+	}
+	
+	void Update () 
+	{
+		if (IsDestroyed ()) 
+		{
+			StartCoroutine(Explode());
+		}
+	}
+
+	IEnumerator Explode()	
+	{
+		gameObject.SetActive(false);
+//		GameObject text = Instantiate (xpText, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity) as GameObject;
+//		text.GetComponent<XPText> ().Show (xp);
+		GameObject explosion = Instantiate (explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+		yield return new WaitForSeconds (0.8f);
+		Destroy (explosion);
+	}
+
+	public void FollowPath(string pathName)
+	{
+		iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "time", 6, "easetype", iTween.EaseType.linear));
+	}
+}
