ea953a2d9b3b88ffc7b262b2373ebe405643916b
NielsB <niels_bross@hotmail.com>
2015-12-10 17:59:17 +0100

    Revert "Revert "tits up""
    This reverts commit 496cfb98b56ac7a1cd122d95baeb4efcdebfa9d2.

---
+++ Assets/Scripts/UI/XPText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Text))]
+public class XPText : MonoBehaviour 
+{
+	private Text text;
+
+	void Start () 
+	{
+		transform.parent = LevelManager.levelManager.canvas.transform;
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	public void Show(int points)
+	{
+		gameObject.GetComponent<Text> ().text = points + "";
+		StartCoroutine(DestroyText ());
+	}
+
+	IEnumerator DestroyText()
+	{
+		for (int i = 0; i < 20; i++) 
+		{
+			yield return new WaitForSeconds (0.05f);
+			transform.position = new Vector2 (transform.position.x, transform.position.y + 0.5f);
+		}
+			Destroy (gameObject);
+	}
+		
+}
---
+++ Assets/Scripts/Weapons/shooting/ProjectileManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileManager : MonoBehaviour 
+{
+
+	void Start () 
+	{
+	
+	}
+	
+	void Update () 
+	{
+	
+	}
+}
