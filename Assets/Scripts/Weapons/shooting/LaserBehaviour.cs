b4c7656eb09e353c1e5eab3d6c539f659d163727
Daniel Bross <daniel@ikontrol.dk>
2015-12-10 21:36:32 +0100

    Collider added to laser

--- Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
+++ Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
@@ -2,22 +2,26 @@
 using System.Collections;
 
 public class LaserBehaviour : ProjectileBehaviour {
-    [SerializeField]
+    
+	[SerializeField]
     protected float heightModifier;
-	// Use this for initialization
-	void Start () {
+
+	void Start () 
+	{
 	
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
         if (gameObject)
         {
-                transform.localScale -= new Vector3(0, heightModifier, 0);
-            transform.localScale.Normalize();
-            Debug.Log(transform.localScale);          
-            
+            transform.localScale -= new Vector3(0, heightModifier, 0);
+            transform.localScale.Normalize();            
         }
+		/* Adjusts the size of the BoxCollider to fit the scaled sprite */
+		Vector2 S = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+		gameObject.GetComponent<BoxCollider2D>().size = S;
+		gameObject.GetComponent<BoxCollider2D>().offset = new Vector2 ((S.x / 2), 0);
 	}
 
     
--- Assets/Scripts/Weapons/shooting/LaserWeapon.cs
+++ Assets/Scripts/Weapons/shooting/LaserWeapon.cs
@@ -2,15 +2,9 @@
 using System.Collections;
 using System;
 
-<<<<<<< HEAD
+
 public class LaserWeapon : AbstractShooter 
 {
-	
-=======
-public class LaserWeapon : AbstractShooter {
-
-
->>>>>>> bcd280e05da96225d48ae805f828c583b2d34c5e
     public override void AuxShoot(Resource projectilePool, float speed)
     {
 
@@ -18,17 +12,13 @@ public class LaserWeapon : AbstractShooter {
         projectile.transform.position = transform.position;
         projectile.gameObject.SetActive(true);
         projectile.velocity = new Vector2(speed, 0);
-
-       
     }
 
-    // Use this for initialization
     void Start () 
 	{
         Init();
 	}
 	
-	// Update is called once per frame
 	void Update () 
 	{
 	
