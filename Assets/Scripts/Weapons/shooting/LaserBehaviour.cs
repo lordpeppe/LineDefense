1429f74446a30c7c31e997f56285b5cf4dfb0f03
NielsB <niels_bross@hotmail.com>
2015-12-10 20:37:31 +0100

    Fixed lazors

--- Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
+++ Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
@@ -4,18 +4,43 @@ using System.Collections;
 public class LaserBehaviour : ProjectileBehaviour {
     [SerializeField]
     protected float heightModifier;
+
+    bool growing;
 	// Use this for initialization
 	void Start () {
-	
+        growing = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (gameObject)
         {
-                transform.localScale -= new Vector3(0, heightModifier, 0);
-            transform.localScale.Normalize();
-            Debug.Log(transform.localScale);          
+            if (transform.localScale.y < 1)
+            {
+                if (growing)
+                {
+                    transform.localScale += new Vector3(0, heightModifier, 0);
+                    transform.localScale.Normalize();
+
+                }
+            }
+            else
+            {
+                growing = false;
+            }
+            if (transform.localScale.y > 0)
+            {
+                if (!growing)
+                {
+                    transform.localScale -= new Vector3(0, heightModifier, 0);
+                    transform.localScale.Normalize();
+                    Debug.Log(transform.localScale);
+                }
+            }
+            else
+            {
+                growing = true;
+            }
             
         }
 	}
