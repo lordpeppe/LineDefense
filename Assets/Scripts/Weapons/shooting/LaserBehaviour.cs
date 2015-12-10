bcd280e05da96225d48ae805f828c583b2d34c5e
unknown <lordpeppezaltaz22@hotmail.com>
2015-12-10 20:28:53 +0100

    Laser weapon modified

---
+++ Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBehaviour : ProjectileBehaviour {
+    [SerializeField]
+    protected float heightModifier;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject)
+        {
+                transform.localScale -= new Vector3(0, heightModifier, 0);
+            transform.localScale.Normalize();
+            Debug.Log(transform.localScale);          
+            
+        }
+	}
+
+    
+}
