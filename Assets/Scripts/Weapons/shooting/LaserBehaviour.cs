9f1195878978ed612c80c08937d0516c2873488d
NielsB <niels_bross@hotmail.com>
2015-12-12 10:45:27 +0100

    Fixes some merge conflicts

--- Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
+++ Assets/Scripts/Weapons/shooting/LaserBehaviour.cs
@@ -6,26 +6,22 @@ public class LaserBehaviour : ProjectileBehaviour {
 	[SerializeField]
     protected float heightModifier;
 
-<<<<<<< HEAD
-	void Start () 
-	{
-	
-=======
+
     bool growing;
 	// Use this for initialization
 	void Start () {
         growing = false;
->>>>>>> ba28a99658083d012c2d801a23fd5389ac7d3530
+
 	}
 	
 	void Update () 
 	{
         if (gameObject)
         {
-<<<<<<< HEAD
+
             transform.localScale -= new Vector3(0, heightModifier, 0);
             transform.localScale.Normalize();            
-=======
+
             if (transform.localScale.y < 1)
             {
                 if (growing)
@@ -53,7 +49,6 @@ public class LaserBehaviour : ProjectileBehaviour {
                 growing = true;
             }
             
->>>>>>> ba28a99658083d012c2d801a23fd5389ac7d3530
         }
 		/* Adjusts the size of the BoxCollider to fit the scaled sprite */
 		Vector2 S = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
--- Assets/Scripts/Weapons/shooting/LaserWeapon.cs
+++ Assets/Scripts/Weapons/shooting/LaserWeapon.cs
@@ -3,17 +3,9 @@ using System.Collections;
 using System;
 
 
-<<<<<<< HEAD
-public class LaserWeapon : AbstractShooter 
-{
-=======
-	
-
 public class LaserWeapon : AbstractShooter {
 
 
-
->>>>>>> ba28a99658083d012c2d801a23fd5389ac7d3530
     public override void AuxShoot(Resource projectilePool, float speed)
     {
 
